7bd8c0ee859ae0e194c56ab194f8afbefc8700bd
Seth MacPherson <macpherson.seth@gmail.com>
2020-10-05 12:59:09 -0700

    final Touches. Ready to submit

--- js/index.js
+++ js/index.js
@@ -32,14 +32,17 @@ document.addEventListener("keydown", (e) => {
   } else if (e.key === "a") {
     e.target.style.color = "blue";
     e.target.style.border = "8x solid green";
+    e.stopPropagation();
     console.log(e.key);
   } else if (e.key === "m") {
     e.target.style.color = "green";
     e.target.style.border = "6px solid yellow";
+    e.stopPropagation();
     console.log(e.key);
   } else if (e.key === "b") {
     e.target.style.color = "yellow";
     e.target.style.border = "4px solid magenta";
+    e.stopPropagation();
     console.log(e.key);
   } else if (e.key === "d") {
     e.target.style.color = "magenta";
@@ -50,6 +53,9 @@ document.addEventListener("keydown", (e) => {
 });
 
 const navChange = document.querySelector(".nav-container .nav");
+navChange.addEventListener("click", function (e) {
+  e.preventDefault();
+});
 navChange.addEventListener("mouseover", function (e) {
   e.target.style.color = "red";
 });
@@ -65,7 +71,7 @@ picChange.addEventListener("click", function () {
 });
 
 const h1Change = document.querySelector("h1");
-h1Change.addEventListener("click", () => {
+h1Change.addEventListener("click", function () {
   h1Change.textContent = "Are You Sure?";
 });
 
@@ -105,12 +111,6 @@ const allh2 = document.querySelectorAll("h2");
 allh2[1].addEventListener("contextmenu", function (e) {
   e.preventDefault();
   console.log("Dont do that.");
-});
-
-const bodyColorChange = document.querySelector("body");
-console.log(bodyColorChange);
-bodyColorChange.addEventListener("resize", function (e) {
-  e.target.style.backgroundColor = "red";
 });
 
 document.addEventListener("keyup", (e) => {
